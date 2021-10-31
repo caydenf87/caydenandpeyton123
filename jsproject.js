b01ab8f77c98611f56fd66155121663ecde988c4
Cayden Felzien <cfelzien18@georgefox.edu>
2021-10-30 17:09:32 -0700

    adding more interactive elements

--- jsproject.js
+++ jsproject.js
@@ -1,10 +1,14 @@
 let userName;
 let character;
 let characterList;
+let chatColor;
 count = 0;
 
 const gameStart = new Audio("Resources/Roundstart.mp3");
+const chatSound = new Audio("Resources/chatSound.mp3");
+const meeting = new Audio("Resources/emergency.mp3");
 
+$("#chat").hide();
 $("#characters").hide();
 $("#downloadButton").hide();
 characterList = document.getElementsByClassName("character");
@@ -26,10 +30,57 @@ async function hideSelect(event)
     character = event.target;
     style = window.getComputedStyle(character, false)
     let bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    bi = bi.slice(-26);
+    let profImage;
+    if (bi.slice(-26)[0] == "R")
+    {
+        profImage = bi.slice(-26);
+    }
+
+    else if (bi.slice(-27)[0] == "R")
+    {
+        profImage = bi.slice(-27);
+    }
+
+    else if (bi.slice(-28)[0] == "R")
+    {
+        profImage = bi.slice(-28);
+    }
+
+    else if (bi.slice(-29)[0] == "R")
+    {
+        profImage = bi.slice(-29);
+    }
+
+
+    switch (profImage)
+    {
+        case "Resources/characterOne.png":
+            chatColor = "Resources/cyanChat.png";
+            break;
+
+        case "Resources/characterTwo.png":
+            chatColor = "Resources/blueChat.png";
+            break;
+
+        case "Resources/characterThree.png":
+            chatColor = "Resources/greenChat.png";
+            break;
+        
+        case "Resources/characterFour.png":
+            chatColor = "Resources/whiteChat.png";
+            break;
+        
+        case "Resources/characterFive.png":
+            chatColor = "Resources/pinkChat.png";
+            break;
+
+        case "Resources/characterSix.png":
+            chatColor = "Resources/orangeChat.png";
+            break;
+    }
 
     let profile = document.createElement("img");
-    profile.src = bi;
+    profile.src = profImage;
     profile.height = 150;
     profile.width = 120;
     profile.style.float = "right";
@@ -127,7 +178,65 @@ async function showDownload()
     $("#content").append(download);
     await sleep(9000);
     $(download).remove();
+    chat();
 }
+
+
+async function chat()
+{
+    let emergency = document.createElement("img");
+    emergency.src = "Resources/emergencyMeeting.jpg";
+    emergency.alt = "meeting";
+    emergency.width = "1500";
+    emergency.height = "550";
+    $("#content").append(emergency);
+    $(emergency).hide();
+    meeting.play();
+    $(emergency).slideDown("slow");
+    await sleep(3000);
+    $(emergency).remove();
+
+    $("#chat").show();
+    let comment = document.createElement("div");
+    comment.id = "otherCommentOne";
+    comment.innerText = "Red was kind of sus"
+
+
+    let commentTwo = document.createElement("div");
+    commentTwo.id = "otherCommentTwo";
+    commentTwo.innerText = "What were you doing " + userName + "?";
+
+    await sleep(2000);
+    chatSound.play();
+    $("#chat").append(comment);
+    await sleep(4000);
+    chatSound.play();
+    $("#chat").append(commentTwo);
+    await sleep(2000);
+
+    let chatBox = document.createElement("input");
+    chatBox.id = "chatBox";
+    chatBox.type = "text";
+    $("#chat").append(chatBox);
+
+    let chatButton = document.createElement("button");
+    chatButton.id = "chatButton";
+    chatButton.onclick = submitChat;
+    $("#chat").append(chatButton);
+}
+
+function submitChat()
+{
+    let comment = document.createElement("div");
+    comment.id = "myComment";
+    comment.innerText = $("#chatBox").val();
+    comment.style.backgroundImage = "url(" + chatColor + ")";
+    $("#chatBox").remove();
+    $("#chatButton").remove();
+    chatSound.play();
+    $("#chat").append(comment);
+}
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
